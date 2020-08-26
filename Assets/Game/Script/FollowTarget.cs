10e72e2b67dccb3cda1ddc9cb25c509cb2d0ac9d
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-26 13:57:56 +0200

    Changed from driving on ice to driving normally

---
+++ Assets/Game/Script/FollowTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 offset = Vector3.zero;
+
+    private float _velocity;
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void Start()
+    {
+        _transform.position = target.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.TransformPoint(offset);
+        //_transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
